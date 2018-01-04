ce8a1a608e6aa024196ada859a3b462940f9db9a
Vasil Gramov <gramovv@gmail.com>
2018-01-04 15:10:57 +0200

    added sql seeds

---
+++ src/main/resources/users.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO `users` (`id`, `is_account_non_expired`, `is_account_non_locked`, `is_credentials_non_expired`, `is_enabled`, `first_name`, `last_name`, `password`, `username`) VALUES
+	(1, b'1', b'1', b'1', b'1', 'admin', 'admin', '$2a$10$I2xJiirSk1PyhJnWEzaPTe0KNNRJgk.OwNEywUwUWAq4MMzG8NSMS', 'admin'),
+	(2, b'1', b'1', b'1', b'1', 'vladix', 'vladix', '$2a$10$pIL0K5spBD5Jip7DcqvDXO/X.UoVmL3YChj1nLQJdBVtjM7LlzLHq', 'vladix');
